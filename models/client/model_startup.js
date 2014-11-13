cb5a6c3baa099a20ce9b8f11837cb0ad3e334ee0
Moshe Holtzberg <moholtzberg@gmail.com>
2014-11-13 09:47:19 -0500

    changed remote call from server to client

---
+++ models/client/model_startup.js
@@ -0,0 +1,25 @@
+Meteor.startup( function(){
+	
+	Meteor.call("getModels", last_update, function(e, r){
+		if (!e && r) {
+			var a = JSON.parse(r);
+			console.log("----------> Total models retrieved from DG " + a.length)
+		
+			for (var i=0; i < a.length; i++) {
+				cust = Models.findOne( {dg_model_id: a[i].dg_model_id} )
+				if (cust) {
+					if ((cust.dg_last_update < a[i].dg_last_update) || !cust.dg_last_update) {
+						console.log("-----------> Updating model " + a[i].model_number + " was updated on " + new Date(a[i].dg_last_update).toLocaleDateString())
+						Models.update({_id: cust._id}, {$set: a[i]})
+					};
+				} else {
+					console.log("-----------> Entering model " + a[i].model_number + " was not found in the db")
+					Models.insert({dg_model_id: a[i].dg_model_id, model_make_id: a[i].model_make_id, model_number: a[i].model_number, model_description: a[i].model_description, model_active: a[i].model_active, dg_last_update: a[i].dg_last_update})
+				}
+			}
+			Modules.update({slug: "models"}, {$set: {last_update: new Date()}})
+			console.log("----------> " + "Done updating models")
+		}	
+	});
+	
+})
--- models/server/models_server.js
+++ models/server/models_server.js
@@ -1,7 +1,6 @@
 Meteor.methods({
 	
 	getModels: function (time) {
-		console.log(time)
 		var v = HTTP.get("http://54.164.222.183:3000/models.json?last_update="+time);
 		if (v.statusCode === 200) {
 			return v.content;
@@ -48,34 +47,33 @@ Meteor.startup( function(){
 	
 	Models._ensureIndex( {dg_model_id: 1})
 	
-});
+	if (!Modules.findOne({slug: "models"})) {
+		Modules.insert({name: "Models", slug: "models", icon: "fa-user", active: false, admin_only: true, last_update: new Date()})
+		var last_update = new Date("01/01/2004")
+	}	else {
+		var last_update = Modules.findOne({slug: "models"}).last_update
+	}
 
-if (!Modules.findOne({slug: "models"})) {
-	Modules.insert({name: "Models", slug: "models", icon: "fa-user", active: false, admin_only: true, last_update: new Date()})
-	var last_update = new Date("01/01/2004")
-}	else {
-	var last_update = Modules.findOne({slug: "models"}).last_update
-}
-
-Meteor.call("getModels", last_update, function(e, r){
-	console.log(last_update)
-	if (!e && r) {
-		var a = JSON.parse(r);
-		console.log("----------> Total models retrieved from DG " + a.length)
-		
-		for (var i=0; i < a.length; i++) {
-			cust = Models.findOne( {dg_model_id: a[i].dg_model_id} )
-			if (cust) {
-				if ((cust.dg_last_update < a[i].dg_last_update) || !cust.dg_last_update) {
-					console.log("-----------> Updating model " + a[i].model_number + " was updated on " + new Date(a[i].dg_last_update).toLocaleDateString())
-					Models.update({_id: cust._id}, {$set: a[i]})
-				};
-			} else {
-				console.log("-----------> Entering model " + a[i].model_number + " was not found in the db")
-				Models.insert({dg_model_id: a[i].dg_model_id, model_make_id: a[i].model_make_id, model_number: a[i].model_number, model_description: a[i].model_description, model_active: a[i].model_active, dg_last_update: a[i].dg_last_update})
-			}
-		}
-		Modules.update({slug: "models"}, {$set: {last_update: new Date()}})
-		console.log("----------> " + "Done updating models")
-	}	
+	// Meteor.call("getModels", last_update, function(e, r){
+	// 	if (!e && r) {
+	// 		var a = JSON.parse(r);
+	// 		console.log("----------> Total models retrieved from DG " + a.length)
+	// 		
+	// 		for (var i=0; i < a.length; i++) {
+	// 			cust = Models.findOne( {dg_model_id: a[i].dg_model_id} )
+	// 			if (cust) {
+	// 				if ((cust.dg_last_update < a[i].dg_last_update) || !cust.dg_last_update) {
+	// 					console.log("-----------> Updating model " + a[i].model_number + " was updated on " + new Date(a[i].dg_last_update).toLocaleDateString())
+	// 					Models.update({_id: cust._id}, {$set: a[i]})
+	// 				};
+	// 			} else {
+	// 				console.log("-----------> Entering model " + a[i].model_number + " was not found in the db")
+	// 				Models.insert({dg_model_id: a[i].dg_model_id, model_make_id: a[i].model_make_id, model_number: a[i].model_number, model_description: a[i].model_description, model_active: a[i].model_active, dg_last_update: a[i].dg_last_update})
+	// 			}
+	// 		}
+	// 		Modules.update({slug: "models"}, {$set: {last_update: new Date()}})
+	// 		console.log("----------> " + "Done updating models")
+	// 	}	
+	// });
+	
 });
