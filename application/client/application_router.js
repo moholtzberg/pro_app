becc3dfc651d136a2c51768945f89eaae702cc46
Moshe Holtzberg <moholtzberg@gmail.com>
2014-11-18 00:28:11 -0500

    cleaned up application_router

--- application/client/application_router.js
+++ application/client/application_router.js
@@ -1,17 +1,6 @@
 Router.route('/', {
 	// layoutTemplate: "application_layout",
 	template: 'home',
-	// waitOn: function(){
-	// 	if (Meteor.user()) {
-	// 		this.subscribe("Customers").wait()
-	// 		this.subscribe("Equipments").wait()
-	// 		this.subscribe("Models").wait()
-	// 		this.subscribe("Makes").wait()
-	// 		this.subscribe("Leases").wait()
-	// 		this.subscribe("Users").wait()
-	// 		this.subscribe("Contacts").wait()
-	// 	}
-	// },
 	action: function() {
 		if (this.ready()) {
 			this.render("nav", {to: "nav"});
@@ -27,21 +16,7 @@ Router.configure({
 	loadingTemplate: "loading",
 	waitOn: function(){
 		if (Meteor.user()) {
-			this.subscribe("Customers").wait()
-			this.subscribe("Equipments").wait()
-			this.subscribe("Models").wait()
-			this.subscribe("Makes").wait()
-			this.subscribe("Leases").wait()
-			this.subscribe("Users").wait()
-			this.subscribe("Contacts").wait()
+			this.subscribe("Modules").wait()
 		}
 	}
-	// onRun: function() {
-	// 	if (!Meteor.loggingIn() && !Meteor.user()) {
-	// 		this.redirect('/login');
-	// 	} else {
-	// 		this.render()
-	// 		this.next
-	// 	}
-	// }
 });
