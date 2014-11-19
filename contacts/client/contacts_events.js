270562246daad9b4ce4efead97d927d91743e91e
Moshe Holtzberg <moholtzberg@gmail.com>
2014-11-19 13:12:47 -0500

    Updated IT company on contacts form
    also removed email validator from contacts form

--- contacts/client/contacts_events.js
+++ contacts/client/contacts_events.js
@@ -13,14 +13,15 @@ Template.contacts_form.events({
 		var it_happy = $("select#it_happy").val()
 		var it_use = $("select#it_use").val()
 		var it_notes = $("textarea#it_notes").val()
-		if (re.test(email)) {
+		var it_company = $("#input#it_company").val()
+		// if (re.test(email)) {
 				$("form#contact_form")[0].reset()
 				$('#modal1').modal('hide')
 				Contacts.insert({customer_id: customer_id, group_id: group_id, first_name: first_name, last_name: last_name, email: email, phone: phone, active: true})
-				Customers.update({_id: customer_id}, {$set: {it: {it_outsource: it_oustource, it_happy: it_happy, it_use: it_use, it_notes: it_notes}}})
-		} else {
-			Session.set("error", "Email is invalid");
-		};
+				Customers.update({_id: customer_id}, {$set: {it: {it_outsource: it_oustource, it_happy: it_happy, it_use: it_use, it_company: it_company, it_notes: it_notes}}})
+		// } else {
+		// 	Session.set("error", "Email is invalid");
+		// };
 	}
 
 })
